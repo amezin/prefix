96820c36923d1077fa8f6cb4c13cada253db8d73
Aleksandr Mezin <mezin.alexander@gmail.com>
2018-03-22 17:57:52 +0600

    tests: update to latest lib versions

--- tests/test_prefix.py
+++ tests/test_prefix.py
@@ -16,13 +16,13 @@ def workspace(tmpdir, download_cache_dir):
 def test_glib(workspace):
     prefix.build_cmake('zlib', url='https://zlib.net/zlib-1.2.11.tar.gz', workspace=workspace)
     prefix.build_autotools('libffi', url='http://sourceware.org/pub/libffi/libffi-3.2.1.tar.gz', workspace=workspace)
-    prefix.build_autotools('gettext', url='https://ftp.gnu.org/pub/gnu/gettext/gettext-0.19.8.tar.xz', workspace=workspace)
-    prefix.build_autotools('pcre', url='https://ftp.pcre.org/pub/pcre/pcre-8.41.tar.bz2', workspace=workspace,
+    prefix.build_autotools('gettext', url='https://ftp.gnu.org/pub/gnu/gettext/gettext-0.19.8.1.tar.xz', workspace=workspace)
+    prefix.build_autotools('pcre', url='https://ftp.pcre.org/pub/pcre/pcre-8.42.tar.bz2', workspace=workspace,
                            deps=['zlib'], extra_configure_args=['--enable-unicode-properties'])
 
     deps = ['zlib', 'libffi', 'gettext', 'pcre']
     if sys.platform == 'linux':
-        prefix.build_autotools('util-linux', url='https://www.kernel.org/pub/linux/utils/util-linux/v2.31/util-linux-2.31.1.tar.xz', workspace=workspace)
+        prefix.build_autotools('util-linux', url='https://www.kernel.org/pub/linux/utils/util-linux/v2.32/util-linux-2.32.tar.xz', workspace=workspace)
         deps += ['util-linux']
 
-    prefix.build_autotools('glib', url='https://download.gnome.org/sources/glib/2.55/glib-2.55.2.tar.xz', workspace=workspace, deps=deps)
+    prefix.build_autotools('glib', url='https://download.gnome.org/sources/glib/2.56/glib-2.56.0.tar.xz', workspace=workspace, deps=deps)
